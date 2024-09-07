b3c10731018f6d247977351319222cba5596696c
yooo <micuenta@gmail.com.mx>
2024-09-06 19:59:23 -0600

    test suma factorizado y funcionando

--- src/main/java/mx/com/qtx/calc/test/CalculadoraTests.java
+++ src/main/java/mx/com/qtx/calc/test/CalculadoraTests.java
@@ -5,6 +5,10 @@ import mx.com.qtx.calc.servicios.Calculadora;
 public class CalculadoraTests {
 
 	public static void main(String[] args) {
+		test_suma();
+	}
+
+	private static void test_suma() {
 		Calculadora miMaizro = new Calculadora();
 		int resultado= (int) miMaizro.sumar(34, 69);
 		System.out.println("EL resultado de la suma "+resultado);
