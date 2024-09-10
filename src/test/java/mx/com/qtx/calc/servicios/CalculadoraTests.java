f04ff02e73b74d8bcfc187bbc2b2d6f8a6da0949
yooo <micuenta@gmail.com.mx>
2024-09-10 17:21:11 -0600

    pruebas test restas
    suma
    resta
    multiplicacion

---
+++ src/test/java/mx/com/qtx/calc/servicios/CalculadoraTests.java
@@ -0,0 +1,47 @@
+package mx.com.qtx.calc.servicios;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraTests {
+
+	@Test
+	void testSumar() {
+		//dados
+		int num1=1500;
+		int num2=1500;
+		//cuand0
+		 Calculadora miMaizoro= new Calculadora();
+		 int suma = miMaizoro.sumar(num1, num2);
+		 //entoinces
+		 assertEquals(3000,suma,"No fue cierto");
+	}
+
+	@Test
+	void testRestar() {
+		//dados
+				int num1=1500;
+				int num2=1500;
+				//cuand0
+				 Calculadora miMaizoro= new Calculadora();
+				 int suma = miMaizoro.restar(num1, num2);
+				 //entoinces
+				 assertEquals(0,suma,"No fue cierto");
+	}
+
+	@Test
+	void testMultitplicar() {
+		//dados
+				int num1=1500;
+				int num2=2;
+				//cuand0
+				 Calculadora miMaizoro= new Calculadora();
+				 int suma = miMaizoro.multitplicar(num1, num2);
+				 //entoinces
+				 assertEquals(3000,suma,"No fue cierto");
+	}
+
+}
